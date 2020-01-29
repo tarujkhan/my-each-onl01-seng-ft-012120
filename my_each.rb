68411c5e78d2024860647b7aca193c2f9b6688d5
taroj khan <tarujkhan@yahoo.com>
2020-01-29 20:43:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -18,11 +18,12 @@ def my_each(collection)
  # collection = []
   my_each(collection) do |i|
 i = i + 1 
-break
+
     puts i
   end
  # collection = []
 while i < array.length
+break
 yield (array[i])
 
 end
