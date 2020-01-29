34fa57206de952e7eb974750ac5abcd54c87cb59
taroj khan <tarujkhan@yahoo.com>
2020-01-29 20:38:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,29 @@
-def my_each # put argument(s) here
+#def my_each # put argument(s) here
   # code here
+#end
+
+#i = 0 
+#while i < array.length
+#yield (array[i])
+#i = i + l 
+#end
+#array
+
+
+
+
+def my_each(collection)
+  block_given?
+  i = 0 
+ # collection = []
+  my_each(collection) do |i|
+i = i + 1 
+    puts i
+  end
+ # collection = []
+while i < array.length
+yield (array[i])
+
+end
+array
 end
