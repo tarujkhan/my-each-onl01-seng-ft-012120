44fb46ad8fb72cad2a64d5ce5fa94febf97dc35a
taroj khan <tarujkhan@yahoo.com>
2020-01-29 20:46:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -15,16 +15,12 @@
 def my_each(collection)
   block_given?
   i = 0 
- # collection = []
   my_each(collection) do |i|
 i = i + 1 
-
     puts i
   end
- # collection = []
 while i < array.length
 yield (array[i])
-
 end
 array
 end
