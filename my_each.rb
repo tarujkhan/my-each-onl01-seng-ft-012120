b6f62266d22bdfe42602f6687058610635819b58
taroj khan <tarujkhan@yahoo.com>
2020-01-29 20:59:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,22 +1,9 @@
-#def my_each # put argument(s) here
-  # code here
-#end
-
-#i = 0 
-#while i < array.length
-#yield (array[i])
-#i = i + l 
-#end
-#array
 
 
 
 
 def my_each(collection)
- # block_given?
   i = 0 
-  #my_each(collection) do |i|
-
 while i < array.length
 yield (collection[i])
 i = i + 1 
